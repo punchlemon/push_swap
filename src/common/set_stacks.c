c046cd75207c0b219405ef87ec7b88114c330b23
Reiji Tanaka <retanaka@c3r3s8.42tokyo.jp>
2024-06-29 19:25:18 +0900

    handle single argment

--- src/common/set_stacks.c
+++ src/common/set_stacks.c
@@ -16,12 +16,31 @@
 void	set_stacks(t_stacks *stacks, int argc, char **argv)
 {
 	int	iter;
+	int	argc_flag;
 
+	argc_flag = 0;
 	if (argc < 2)
 		return ;
-	while (stacks->size < (size_t)(argc - 1))
+	if (argc == 2)
 	{
-		iter = ft_atoi(argv[++stacks->size]);
+		argv = ft_split(argv[1], ' ');
+		argc = 0;
+		while (argv[argc])
+			argc++;
+		stacks->size = 0;
+		argc_flag = 1;
+	}
+	else
+		stacks->size = 1;
+	while (stacks->size < (size_t)(argc))
+	{
+		iter = ft_atoi(argv[stacks->size++]);
 		add_tail_stack(stacks->a, create_num(iter));
+	}
+	if (argc_flag)
+	{
+		for (int i = 0; argv[i]; i++)
+			free(argv[i]);
+		free(argv);
 	}
 }

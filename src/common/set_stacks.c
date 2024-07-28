3a161345bffdf8f927b3d68d57592da072d00e78
punchlemon <tsukiwahigashini0410@gmail.com>
2024-07-28 17:46:59 +0900

    modified set_stacks

--- src/common/set_stacks.c
+++ src/common/set_stacks.c
@@ -24,8 +24,10 @@ int	ft_atoi_revised(const char *str, int *num)
 	if (*str == '+' || *str == '-')
 		str++;
 	*num = 0;
-	while (*str >= '0' && *str <= '9')
+	while (*str)
 	{
+		if (*str < '0' || *str > '9')
+			return (1);
 		if (sign == 1 && (*num > M || (*num == M && *str > '7')))
 			return (1);
 		else if (sign == -1 && (*num > M || (*num == M && *str > '8')))
@@ -60,44 +62,19 @@ int	check_dup_stacks(t_stacks *stacks)
 	return (0);
 }
 
-int	two_args_stacks(t_stacks *stacks, int argc, char **argv)
-{
-	int	i;
-	int	num;
-
-	argv = ft_split(argv[1], ' ');
-	argc = 0;
-	while (argv[argc])
-		argc++;
-	stacks->size = 0;
-	while (stacks->size < (size_t)(argc))
-	{
-		if (ft_atoi_revised(argv[stacks->size++], &num))
-			return (1);
-		add_tail_stack(stacks->a, create_num(num));
-	}
-	i = -1;
-	while (argv[++i])
-		free(argv[i]);
-	free(argv);
-	return (check_dup_stacks(stacks));
-}
-
 int	set_stacks(t_stacks *stacks, int argc, char **argv)
 {
 	int	num;
 
-	if (argc < 2)
-		return (1);
-	if (argc == 2)
-		return (two_args_stacks(stacks, argc, argv));
+	if (argc <= 2)
+		return (delete_stacks(stacks), 1);
 	else
 	{
 		stacks->size = 1;
 		while (stacks->size < (size_t)(argc))
 		{
 			if (ft_atoi_revised(argv[stacks->size++], &num))
-				return (1);
+				return (delete_stacks(stacks), 1);
 			add_tail_stack(stacks->a, create_num(num));
 		}
 	}
